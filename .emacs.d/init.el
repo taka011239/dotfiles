a8c8fd52245da4e33c16372b9a33ea6c655b54a9
taka011239 <taka011239@gmail.com>
2014-03-21 14:26:45 +0900

    add marmalade repo.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,12 +19,12 @@
 (when (require 'package nil t)
 
 ;; MELPA
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+ (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 ;; Marmalade
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+ (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;; Only MELPA
-  (setq package-archives (list '("melpa" . "http://melpa.milkbox.net/packages/")))
+;;  (setq package-archives (list '("melpa" . "http://melpa.milkbox.net/packages/")))
 
   (package-initialize)
 
