a2d94b6e05ebd95154eb9655b015dcf001373036
taka011239 <taka011239@gmail.com>
2015-12-11 03:09:59 +0900

    add jsx support

---
+++ .emacs.d/conf/inits/57-web-mode.el
@@ -0,0 +1,13 @@
+(autoload 'web-mode "web-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
